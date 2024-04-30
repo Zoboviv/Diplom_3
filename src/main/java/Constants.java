1a4bdacda58a544008515531870ef5486d6fbce6
Igor Zobov <zobov.i.v@yandex.ru>
2024-04-30 16:25:26 +0300

    Коммит_1

---
+++ src/main/java/Constants.java
@@ -0,0 +1,3 @@
+public class Constants {
+    public static final String WEBSITE = "https://stellarburgers.nomoreparties.site/";
+}
---
+++ src/test/java/AncestorTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AncestorTest {
+
+    WebDriver driver=getWebDriver(true);//true для FirefoxDriver
+
+    public static WebDriver getWebDriver(boolean useFirefox) {
+        if (useFirefox) {
+            return new FirefoxDriver();
+        } else {
+            return new ChromeDriver();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        driver.get(Constants.WEBSITE);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
