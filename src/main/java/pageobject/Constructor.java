af1d60d0d1064f468afbc092c259b1fb6a2dc9a3
Igor Zobov <zobov.i.v@yandex.ru>
2024-04-30 20:04:54 +0300

    Коммит_2

---
+++ src/main/java/pageobject/Constructor.java
@@ -0,0 +1,16 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class Constructor {
+    public WebDriver driver;
+
+    //конструктор класса
+    public Constructor(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+}
